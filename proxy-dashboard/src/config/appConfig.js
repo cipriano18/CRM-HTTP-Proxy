424f9e135b14376a1c4d437fdaeebfd1e0dabf47
CiprianoRiveraEscobar <ciprianoantonioriveraescobar@gmail.com>
2026-06-07 22:38:58 -0600

    bug de carga css

--- proxy-dashboard/src/config/appConfig.js
+++ proxy-dashboard/src/config/appConfig.js
@@ -1,7 +1,8 @@
 export const appConfig = {
   dashboardPort: 3000,
   defaultRefreshIntervalMs: 3000,
+
   metricsEndpoint:
     import.meta.env.VITE_PROXY_METRICS_URL ??
-    'http://192.168.1.5:8090/api/dashboard/metrics',
+    'http://192.168.1.12:8090/api/dashboard/metrics',
 }
